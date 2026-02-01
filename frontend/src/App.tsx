6cc1d2c110b2ffce1282b073dbaf8b2f09209205
Ahmed <ahmedmursaljamac2004@gmail.com>
2026-02-01 13:20:47 +0300

    alot of change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,32 @@
 // frontend/src/App.tsx
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import LoginPage from "./auth/LoginPage";
 import RegisterPage from "./auth/RegisterPage";
 import ForgotPasswordPage from "./auth/ForgotPasswordPage";
 import ResetPasswordPage from "./auth/ResetPasswordPage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+
 import AdminDashboardPage from "./pages/AdminDashboardPage";
 import UserDashboardHomePage from "./pages/UserDashboardHomePage";
 import UserRequestNewPage from "./pages/UserRequestNewPage";
 import UserRequestsPage from "./pages/UserRequestsPage";
 import LicenseCardPreviewPage from "./pages/LicenseCardPreviewPage";
+
 import PaymentPage from "./features/PaymentPage";
 
 export default function App() {
   return (
     <Routes>
+      {/* Default */}
       <Route path="/" element={<Navigate to="/login" replace />} />
 
+      {/* Auth */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-      // Example
-      <Route path="/app/user/requests/:id/card" element={<LicenseCardPreviewPage />} />
 
       {/* USER */}
       <Route
@@ -54,6 +57,15 @@ export default function App() {
       />
 
       <Route
+        path="/app/user/requests/:id/card"
+        element={
+          <ProtectedRoute role="user">
+            <LicenseCardPreviewPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
         path="/app/user/pay/:id"
         element={
           <ProtectedRoute role="user">
@@ -72,6 +84,7 @@ export default function App() {
         }
       />
 
+      {/* Catch-all */}
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
